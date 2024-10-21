453537cf0f1ac96bdf9b3c1e6e5e1db91fa34889
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2024-10-21 17:52:14 +0200

    completato esercizio bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -6,13 +6,29 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
+//* Dichiara la funzione qui.
+//* Invoca la funzione qui e stampa il risultato in console
+//*Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
 
 
-// Dichiara la funzione qui.
+function salutoNome(nome)
+{
+    let now = new Date();
+    let ora = now.getHours();
+    if(ora >= 0 && ora < 13)
+    {
+        return "buongiorno " + nome;
+    } 
+    else if(ora >= 13 && ora < 17 )
+    {
+        return "buon pomeriggio " + nome;
+    }
+    else
+    {
+        return "buonasera " + nome;
+    }
 
+}
 
-// Invoca la funzione qui e stampa il risultato in console
+console.log(salutoNome(name));
 
-
-
-//Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
